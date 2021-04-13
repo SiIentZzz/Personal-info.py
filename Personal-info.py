5173ff5317376a42f3d2e464af1502b8ad6382be
SiIentZzz <76645232+SiIentZzz@users.noreply.github.com>
2021-04-13 16:38:37 +0800

    Add files via upload

---
+++ Personal-info.py
@@ -0,0 +1,8 @@
+name = "Aeron Gabriel Montalbo\n"
+Age = 20
+Address = "San Carlos, Rosario, Batangas\n"
+Birthday = "November 23, 2000\n"
+Course = "B.S. Information Technology\n"
+school = "De La Salle University - Dasmarinas\n"
+
+print("\n\n\nName: " + name + "Age: " + str(Age) + "\nAddress: " + Address + "Birthday: " + Birthday + "Course: " +Course + "School: " + school)
