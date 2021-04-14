1b761de0ab18d4ba79a9d09ee6bdbeccb5d00b90
SiIentZzz <76645232+SiIentZzz@users.noreply.github.com>
2021-04-14 15:59:07 +0800

    Update Personal-info.py

--- Personal-info.py
+++ Personal-info.py
@@ -1,8 +1,8 @@
-name = "Aeron Gabriel Montalbo\n"
 Age = 20
 Address = "San Carlos, Rosario, Batangas\n"
-Birthday = "November 23, 2000\n"
-Course = "B.S. Information Technology\n"
-school = "De La Salle University - Dasmarinas\n"
+firstn = "Aeron Gabriel "
+lastn = "Montalbo"
+space = " "
 
-print("\n\n\nName: " + name + "Age: " + str(Age) + "\nAddress: " + Address + "Birthday: " + Birthday + "Course: " +Course + "School: " + school)
+
+print("\n\n\n" + firstn + space + lastn + space + str(Age) + space + Address)
